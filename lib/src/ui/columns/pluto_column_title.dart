e1f35c689121d59b2d50bf56c523533ef52bf8c2
leonardo1952 <lo88980@gmail.com>
2023-09-12 17:41:55 -0300

    .

--- lib/src/ui/columns/pluto_column_title.dart
+++ lib/src/ui/columns/pluto_column_title.dart
@@ -178,20 +178,21 @@ class PlutoColumnTitleState extends PlutoStateWithChange<PlutoColumnTitle> {
     return Stack(
       children: [
         Positioned(
-          left: 0,
-          right: 0,
-          child: widget.column.enableColumnDrag
+            left: 0,
+            right: 0,
+            child: Text(
+                'meu teste') /*widget.column.enableColumnDrag
               ? _DraggableWidget(
                   stateManager: stateManager,
                   column: widget.column,
                   child: columnWidget,
                 )
-              : columnWidget,
-        ),
+              : columnWidget,*/
+            ),
         if (showContextIcon)
           Positioned.directional(
             textDirection: stateManager.textDirection,
-            end: 10,
+            end: 5,
             child: enableGesture
                 ? Container(
                     color: Colors.red,
